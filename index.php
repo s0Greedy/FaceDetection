e6c057c8723091ed155dd9b101d32d9300eca8c1
Muhammad Fachri Ridha <fachri213ridha@gmail.com>
2024-11-26 14:04:21 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compitable" content="ie=edge">
+    <title>CRUD (Create, Read, Update & Delete) Mahasiswa</title>
+</head>
+<body>
+    <h1>CRUD Mahasiswa</h1>
+     <a href="#">Mahasiswa</a>
+</body>
+
+</html>
